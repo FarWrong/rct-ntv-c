3e72b520f30596389615890950b407af82de9b1d
nickci2002 <nickci2002@gmail.com>
2024-03-05 12:50:57 -0500

    Revert "Updated login screen"
    This reverts commit e02da157e1312e1ce22b3430e7f83922ae03c29c.

--- app/components/textbox.tsx
+++ app/components/textbox.tsx
@@ -6,34 +6,25 @@ import { useState } from 'react';
 /**
  * The textbox file creates textboxes that take in text input
  * 
- * @param {string} placeholder - Text input when there is no text input
- * @param {string} value - Variable to pass input text to
- * @param {function()} onChangeText - Function to run when text is changed
- * @param {boolean} secure - Check if the text field should be secure
+ * @param {string} title - Text input when there is no text input
  * @param {StyleProp} style - Allow custom styles to be passed
  */
 interface TextboxProps {
-    placeholder?: string;
-    value?: string;
-    onChangeText?: (string) => void;
-    secure?: boolean;
+    title?: string;
     style?: StyleProp<ViewStyle>;
 }
 
-export const TextBox: React.FC<TextboxProps> = ({ placeholder, value, onChangeText, secure, style }) => {
+export const TextBox: React.FC<TextboxProps> = ({ title, style }) => {
     const { theme } = useTheme();
     // Controls highlighted box feature
     const [focus, setFocus] = useState(false);
     const toggleFocus = () => { setFocus(!focus) }
-    const calcSecureText = () => {return secure}
 
     return (
         <TextInput
             onFocus={toggleFocus} // Become primary color
             onBlur={toggleFocus} // Become secondary color
-            placeholder={placeholder}
-            value={value}
-            onChangeText={onChangeText}
+            placeholder={title}
             style={[
                 {
                     borderColor: (focus ? theme.colors.primary
