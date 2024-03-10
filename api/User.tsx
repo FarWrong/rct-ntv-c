9683d81fa89f0ae24e1e91aacb3af6ba5fceb45c
banmeetkaur1 <bkaur5@pride.hofstra.edu>
2024-03-10 00:48:15 -0500

    added stuff

--- api/User.tsx
+++ api/User.tsx
@@ -10,3 +10,41 @@ export const getUserGender = (context:ApiContextType) => {
   }
   return context;
 };
+
+export interface UserType {
+  name?: string;
+  email?: string;
+  age?: number;
+  gender?: string;
+  weight?: number;
+}
+
+
+export const getUserInfo = async(token: string) => {
+  console.log(token)
+  let userInfo: UserType = {}
+  let errorMessage = "success"
+  try{
+      const response = await fetch('http://127.0.0.1:8000/users/gender', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              "Authorization": "Bearer "  + token
+          },
+          
+          });
+      
+      if (!response.ok) {
+
+          console.error("Failed to fetch token.");
+          return userInfo;
+      }
+        const data = await response.json();
+        
+        userInfo.email = data.email;
+        return userInfo;
+  }catch(error:any){
+      errorMessage = error;
+      return userInfo;
+  }
+}
